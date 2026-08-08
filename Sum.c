4956c15614fcc842465e82621790b9bd0eebf4dc
SAM-AK-ASH <samakashramesh2007@gmail.com>
2026-08-08 20:20:06 +0530

    Add files via upload

---
+++ Sum.c
@@ -0,0 +1,13 @@
+// Add 2 numbers
+
+#include <stdio.h>
+
+void main(){
+    int a,b,c;
+    printf("Enter value of a: ");
+    scanf("%d",&a);
+    printf("Enter value of b: ");
+    scanf("%d",&b);
+    c = a + b;
+    printf("%d\n",c);
+}
