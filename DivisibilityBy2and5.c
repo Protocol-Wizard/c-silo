ffdff3ee08fa874b09ac5b252ca0b8fd89adb26a
SAM-AK-ASH <samakashramesh2007@gmail.com>
2026-08-08 16:56:42 +0530

    Add files via upload

---
+++ DivisibilityBy2and5.c
@@ -0,0 +1,15 @@
+// Divisibility by 2 and 5
+
+#include <stdio.h>
+
+void main(){
+    int a;
+    printf("Enter value of a: ");
+    scanf("%d",&a);
+    if(a%10==0){
+        printf("Yes.\n");
+    }
+    else{
+        printf("No.\n");
+    }
+}
